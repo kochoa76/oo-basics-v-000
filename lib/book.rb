ce63c3c821a26cb963c5102d05c3a92d5a31eb8a
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-22 18:26:35 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book 
-  attr_accessor :author, :page_count, :genre, 
+  attr_accessor :author, :page_count, :genre 
   attr_reader :title 
 
 def initialize(title)
@@ -9,7 +9,17 @@ end
 def turn_page 
   puts "Flipping the page... wow, you read fast!"
 end 
-
 end
 
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
